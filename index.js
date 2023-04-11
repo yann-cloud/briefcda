db90a582086efb5bf7b12ae3c25dbdc21952899f
yann-cloud <obamayann9@gmail.com>
2023-04-11 17:11:47 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('hello world');
+});
+
+app.listen('8082', () =>{
+    console.log('server disponible at port 8082');
+})
